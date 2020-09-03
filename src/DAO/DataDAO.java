62d29fd8a3a1a0c9fdcc3f758be16a94a38c221c
lyeonji <lyeonji@DESKTOP-MQ0MV2Q>
2020-09-04 06:41:23 +0900

    update

--- src/DAO/DataDAO.java
+++ src/DAO/DataDAO.java
@@ -1,5 +1,6 @@
 package DAO;
 
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.ArrayList;
 
@@ -27,11 +28,11 @@ public class DataDAO extends DAOBase {
 
 				while (rs.next()) {
 					Data tmpData = new Data();
-					tmpData.setNo("no");
-					tmpData.setTitle("title");
-					tmpData.setUrl("url");
-					tmpData.setText("text");
-					tmpData.add(tmpData);
+					tmpData.setNo(rs.getInt("no"));
+					tmpData.setTitle(rs.getString("title"));
+					tmpData.setUrl(rs.getString("url"));
+					tmpData.setText(rs.getString("text"));
+					tmpList.add(tmpData);
 				}
 			} catch (Exception e) {
 				System.out.println("list 오류");
@@ -51,7 +52,79 @@ public class DataDAO extends DAOBase {
 		return tmpList;
 	}
 
-	public void insert() {
-		String sql="insert into data values(?, ?, ?, ?)"
+	public void insert(Data newData) {
+		String sql = "insert into data values(?, ?, ?, ?)";
+		PreparedStatement ppst = null;
+
+		if (conn() != null) {
+			try {
+				ppst = conn.prepareStatement(sql);
+				ppst.setInt(1, newData.getNo());
+				ppst.setString(2, newData.getTitle());
+				ppst.setString(3, newData.getUrl());
+				ppst.setString(4, newData.getText());
+				ppst.executeUpdate();
+			} catch (Exception e) {
+				System.out.println("insert 오류");
+			} finally {
+				try {
+					if (conn != null)
+						conn.close();
+					if (ppst != null)
+						ppst.close();
+				} catch (Exception e) {
+					System.out.println("close 오류");
+				}
+			}
+		}
+	}
+
+	public void delete(Data delData) {
+		String sql = "delete from data where title=?";
+		PreparedStatement ppst = null;
+
+		if (conn() != null) {
+			try {
+				ppst = conn.prepareStatement(sql);
+				ppst.setString(1, delData.getTitle());
+			} catch (Exception e) {
+				System.out.println("delete 오류");
+			} finally {
+				try {
+					if (conn != null)
+						conn.close();
+					if (ppst != null)
+						ppst.close();
+				} catch (Exception e2) {
+					System.out.println("close 오류");
+				}
+			}
+		}
+	}
+
+	public void update(String title, String url, String text) {
+		String sql = "update data set url=?, text=? where title=?";
+		PreparedStatement ppst = null;
+
+		if (conn() != null) {
+			try {
+				ppst = conn.prepareStatement(sql);
+				ppst.setString(1, url);
+				ppst.setString(2, text);
+				ppst.setString(3, title);
+				ppst.executeUpdate();
+			} catch (Exception e) {
+				System.out.println("update 오류");
+			} finally {
+				try {
+					if (conn != null)
+						conn.close();
+					if (ppst != null)
+						ppst.close();
+				} catch (Exception e) {
+					System.out.println("close 실패");
+				}
+			}
+		}
 	}
 }
