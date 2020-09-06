3eb9f9da83e09824efe53452574c273982381d1c
lyeonji <lyeonji@DESKTOP-MQ0MV2Q>
2020-09-06 22:22:28 +0900

    user dto create

--- src/DTO/User.java
+++ src/DTO/User.java
@@ -1,5 +1,34 @@
 package DTO;
 
+//	DB table¸í: siteUser
 public class User {
-
+	String id;
+	String pw;
+	String name;
+	String nick;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
 }
